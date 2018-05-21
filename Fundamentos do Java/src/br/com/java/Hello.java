710c20b901968aa2134c4f3bfe1cfb0532cf53b4
Marcoos11 <38111458+Marcoos11@users.noreply.github.com>
2018-05-21 17:02:17 -0300

    Adicionei arquivos via upload

---
+++ Fundamentos do Java/src/br/com/java/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.java;
+
+/**
+ * @author marcos.vmsilva7
+ *Hello World
+ */
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+	}
+
+}
