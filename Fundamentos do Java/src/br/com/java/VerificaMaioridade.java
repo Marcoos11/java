f7f4f8784b5e363594d79472c9f7645d2c235c57
marcos.vmsilva7 <marcos.vmsilva7@TAT0541229W10-1.senacsp.edu.br>
2018-06-06 17:27:21 -0300

    Verificar a maioridade menor de idade e maior

---
+++ Fundamentos do Java/src/br/com/java/VerificaMaioridade.java
@@ -0,0 +1,31 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+public class VerificaMaioridade {
+
+	public static void main(String[] args) {
+		String nome;
+		int idade;
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Digite o seu nome: ");
+		nome = teclado.nextLine();
+		System.out.println("Digite sua idade: ");
+		idade = teclado.nextInt();
+		System.out.println("");
+		System.out.println("______________________");
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+		// Verificação de idade +18
+		if (idade < 18) {
+			System.out.println("Menor de idade");
+			
+		} else {
+			System.out.println("Maior de idade");
+
+		}
+		
+
+	}
+
+}
