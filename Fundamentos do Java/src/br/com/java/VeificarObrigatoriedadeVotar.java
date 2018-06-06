340d2e038629c01a30852ebd1d38d9a630c3ab5d
marcos.vmsilva7 <marcos.vmsilva7@TAT0541229W10-1.senacsp.edu.br>
2018-06-06 17:14:31 -0300

    Obrigatoriedade de Votar

---
+++ Fundamentos do Java/src/br/com/java/VeificarObrigatoriedadeVotar.java
@@ -0,0 +1,31 @@
+package br.com.java;
+
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementFilter;
+
+public class VeificarObrigatoriedadeVotar {
+
+	public static void main(String[] args) {
+		int idade;
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Verificar obrigatoriedade de votar");
+		System.out.print("Digite a sua idade: ");
+		idade = teclado.nextInt();
+		/* Critérios
+		 * Menor que 16 anos - Proibido votar
+		 * 16, 17 ou acima de 70 - Voto facultativo
+		 * Entre 18 e 70 - Obrigatório votar
+		 *     */
+		
+		 if (idade < 16) {
+			 System.out.println("Proibido votar");
+			 
+		 } 	else if (idade == 16 || idade == 17 || idade > 70) {
+	          System.out.println("Voto Facultativo");
+		 }  else {
+             System.out.println("Voto obrigatório");
+	     }
+
+	}
+}
