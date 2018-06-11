5c7b35828519f52a71e81f7eb5e3db117dcaaba4
marcos.vmsilva7 <marcos.vmsilva7@TAT0541229W10-1.senacsp.edu.br>
2018-06-11 15:49:28 -0300

    Jogo do dado

---
+++ Fundamentos do Java/src/br/com/java/JogoDado.java
@@ -0,0 +1,24 @@
+package br.com.java;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogoDado {
+
+	public static void main(String[] args) {
+		char opcao;
+		Scanner teclado = new Scanner(System.in);
+		do {
+		    //a linha abaixo usa a classe Random para criar um objeto que
+			// será usado para gerar números aleatórios;
+			Random dado = new Random();
+			int face = dado.nextInt(6); //gerar números aleatórios (0 a 5) somamos 1 ao resultado
+			//pois não existe a face 0 no dado
+			System.out.println("Face do dado: " + face);
+			System.out.println("Deseja jogar novamente(s/n)? ");
+			opcao = teclado.next().charAt(0);
+		} while(opcao == 's');
+
+	}
+
+}
